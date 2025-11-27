86f24dbf95e9c80b6f18206a64bb5b3b06828e72
anas Abdullfattah <anasAbdullfattah@gmail.com>
2025-11-27 20:33:12 +0000

    to chage from laptop to pc

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock.tick(60)
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
